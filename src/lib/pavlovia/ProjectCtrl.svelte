02789d403e0ad4e4d44611259ca558264e9f8fcb
Todd OST <todd.e.parsons@gmail.com>
2025-12-15 14:40:46 +0000

    ENH: Show current project info

--- src/lib/pavlovia/ProjectCtrl.svelte
+++ src/lib/pavlovia/ProjectCtrl.svelte
@@ -1,7 +1,7 @@
 <script>
     import { DropdownButton } from "$lib/utils/buttons";
     import { getContext, onMount } from "svelte";
-    import { projects } from "./pavlovia.svelte";
+    import { projects, users } from "./pavlovia.svelte";
     import { MenuItem, MenuSeparator, SubMenu } from "$lib/utils/menu";
     import { electron, git } from "$lib/globals.svelte";
     import ManageProjectsDlg from "$lib/dialogs/projects/manage/ManageProjectsDlg.svelte";
@@ -37,11 +37,58 @@
         manageProjectsDlg: false,
         browseProjectsDlg: false
     })
+
+    $effect(() => {
+        // if we have both an experiment file and a user...
+        if (current.experiment?.file?.parent && current.user) {
+            // get git remote
+            git.getRemote(
+                $state.snapshot(current.experiment.file.parent), $state.snapshot(current.user)
+            ).then(
+                remote => new URL(remote)
+            ).then(
+                remote => {
+                    // get name from remote URL
+                    let [_, group, name] = remote.pathname.match(/\/(.+?)\/(.+?)\.git/)
+                    // search GitLab
+                    fetch(
+                        `https://gitlab.pavlovia.org/api/v4/users/${group}/projects?search=${name}&access_token=${current.user.token.access}`
+                    ).then(
+                        resp => resp.json()
+                    ).then(
+                        resp => {
+                            if (resp.length) {
+                                // if we found one, use its details
+                                current.project = resp[0]
+                                // log
+                                console.log(`Loaded project ${group}/${name}`, resp[0])
+                            }
+                            
+                        }
+                    )
+                }
+            )
+        }
+    })
+
+    let label = $derived.by(() => {
+        if (current.project) {
+            if (current.project.owner.username === current.user?.profile?.username) {
+                // if owner is current user, just display name
+                return current.project.path 
+            } else {
+                // otherwise, display group and name
+                return `${current.project.namespace?.path}/${current.project.path}`
+            }
+        } else {
+            // if no project, no label
+            return "No project"
+        }
+    })
 </script>
 
 <DropdownButton
-    label={current.project ? `${current.project.group}/${current.project.name}` : "No project"}
-    icon={current.project ? current.project.avatar_url : undefined}
+    label={label}
     onclick={(evt) => {
         if (current.project) {
             window.open(current.project.remote)
