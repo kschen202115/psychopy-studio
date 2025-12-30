e7e9ff2746c9cb04d78c510e6a94b7b7a041dc4f
Todd Parsons <todd.e.parsons@googlemail.com>
2025-12-30 13:17:38 +0000

    ENH: Add support for custom theme via a CSS file

--- src/lib/utils/Theme.svelte
+++ src/lib/utils/Theme.svelte
@@ -1,16 +1,44 @@
 <script>
-    import { prefs } from "$lib/preferences.svelte"
+    import { prefs } from "$lib/preferences.svelte";
+    import { electron } from "$lib/globals.svelte";
+
+    let url = $derived.by(() => {
+        if (prefs.params.theme.val === "custom") {
+            return `/${prefs.params.customTheme.val}`
+        } else {
+            return `/themes/${prefs.params.theme.val}.css`
+        }
+    })
 </script>
 
 <svelte:head>
-    <svelte:boundary>
+    {#if prefs.params.theme.val === "custom"}
+        <!-- load theme from file if given one -->
+        {#if electron}
+            {#await electron.files.load(prefs.params.customTheme.val) then styling}
+                {@html `<style type='text/css'>\n${styling}\n</style>`}
+            {:catch err}
+                <!-- fallback to psychopy.css if we can't load the custom file -->
+                <link 
+                    rel="stylesheet" 
+                    href="/themes/psychopy.css"
+                >
+            {/await}
+        {:else}
+            <!-- fallback to psychopy.css if in web view (which can't access files by path) -->
+            <link 
+                rel="stylesheet" 
+                href="/themes/psychopy.css"
+            >
+        {/if}
+    {:else}
         <link 
             rel="stylesheet" 
-            href="/themes/{prefs.params.theme.val}.css"
+            href={url}
             onerror={
-                // fallback to psychopy.css
+                // fallback to psychopy.css if theme doesn't load
                 evt => evt.target.href = "/themes/psychopy.css"
             }
         >
-    </svelte:boundary>
+    {/if}
 </svelte:head>
