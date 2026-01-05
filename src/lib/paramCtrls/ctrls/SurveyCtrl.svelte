e4afe3adf353c4b71b048842825752603ba36584
Todd OST <todd.e.parsons@gmail.com>
2026-01-05 13:25:52 +0000

    ENH: Properly authenticate and await SurveyCtrl options

--- src/lib/paramCtrls/ctrls/SurveyCtrl.svelte
+++ src/lib/paramCtrls/ctrls/SurveyCtrl.svelte
@@ -1,6 +1,6 @@
 <script>
     import SingleLineCtrl from "./SingleLineCtrl.svelte"
-    import { CompactButton, IconButton } from "$lib/utils/buttons"
+    import { CompactButton, IconButton, RadioGroup, RadioButton } from "$lib/utils/buttons"
     import { getContext } from "svelte";
     import Dialog from "$lib/utils/dialog/Dialog.svelte";
     import { users } from "$lib/pavlovia/pavlovia.svelte";
@@ -24,40 +24,19 @@
         survey: undefined
     });
 
-    let surveys = $state({
-        available: [],
-        error: undefined
-    });
+    async function getSurveys() {
+        // request from Pavlovia
+        let data = await fetch(
+            "/api/surveys",
+            {
+                headers: current.user.token
+            }
+        ).then(
+            resp => resp.json()
+        )
 
-    async function populateSurveys() {
-        // clear surveys
-        surveys.available = []
-        surveys.error = undefined
-        // populate
-        if (current.user) {
-            // todo: how the hell does fetch work?
-            let resp;
-            try {
-                resp = await fetch(
-                    "https://pavlovia.org/api/v2/surveys",
-                    {
-                        headers: current.user.token
-                    }
-                )
-            } catch (err) {
-                surveys.error = err;
-                return
-            }
-            let data = await resp.json()
-            // if we got any, show them
-            if ("surveys" in data) {
-                for (let survey of data.surveys) {
-                    surveys.available.push(survey)
-                }
-            }
-        }
+        return data.surveys
     }
-
 </script>
 
 <SingleLineCtrl 
@@ -77,7 +56,6 @@
 <Dialog
     id=browse-surveys
     title="Pavlovia surveys"
-    onopen={populateSurveys}
     buttons={{
         OK: (evt) => param.val = selected.survey.surveyId,
         CANCEL: (evt) => {}
@@ -89,36 +67,30 @@
 >
     <div class=container>
         <p>
-            Below are all of the surveys linked to your Pavlovia account - select the one you want and press OK to add its ID.
+            Below are all of the surveys linked to your Pavlovia account - select the one you want and press OK to add its ID. You can view and manage your Pavlovia surveys <a href="https://pavlovia.org/dashboard?tab=4" target="_blank">here</a>
         </p>
-        <div class=choice-group>
-            {#each surveys.available as survey}
-                <button
-                    class=survey-option
-                    onclick={(evt) => selected.survey = survey}
-                    class:selected={selected.survey && selected.survey.surveyId === survey.surveyId}
-                >
-                    {survey.surveyName}
-                </button>
-            {/each}
-            {#if surveys.error}
-            <p class=error>
-
-            </p>
-            {/if}
-        </div>
-        <div class=ctrls>
-            <IconButton
-                label="Manage surveys on Pavlovia"
-                icon="/icons/rbn-pavlovia.svg"
-                onclick={(evt) => window.open("https://pavlovia.org/dashboard?tab=4")}
-            />
-            <IconButton
-                label="Refresh available surveys"
-                icon="/icons/btn-refresh.svg"
-                onclick={populateSurveys}
-            />
-        </div>
+        <RadioGroup
+            bind:value={selected.survey}
+        >
+            <div class=choice-group>
+                {#await getSurveys()}
+                    Loading surveys...
+                {:then surveys}
+                    {#each surveys as survey}
+                        {console.log(survey)}
+                        <RadioButton
+                            value={survey}
+                            label={survey.surveyName}
+                            tooltip={survey.description}
+                        />
+                    {/each}
+                {:catch err}
+                <p class=error>
+                    {err}
+                </p>
+                {/await}
+            </div>
+        </RadioGroup>
     </div>
 </Dialog>
 
@@ -139,28 +111,9 @@
         align-items: stretch;
         flex-grow: 1;
         overflow-y: auto;
-    }
-
-    .survey-option {
-        background-color: transparent;
-        border: 1px solid transparent;
-        padding: .5rem;
-    }
-    .survey-option:hover,
-    .survey-option:focus {
-        border-color: var(--blue);
-    }
-    .survey-option.selected {
-        background-color: var(--blue);
-        color: var(--text-on-blue);
-    }
-
-    .ctrls {
-        display: flex;
-        flex-direction: row;
+        max-width: 65rem;
         gap: .5rem;
-        padding: .5rem;
-        padding-bottom: 1.5rem;
+        padding: 1rem;
     }
 
     p.error {
